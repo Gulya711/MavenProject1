d37a9b50eb511d46e460a3333616043799fa3aa1
Juli Diva <juli@golo.com>
2022-05-03 06:57:48 -0400

    first commit

---
+++ src/test/java/AscendingSequenceTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder (MethodOrderer.OrderAnnotation.class)
+public class AscendingSequenceTest {
+    @Order(1)
+    @Test
+    public void testAscendingSequenceHappyPath(){
+        // 0,1,2,3,4,5
+        int start = 0;
+        int end =5;
+        int step =1;
+        int[] expectedResult = {0,1,2,3,4,5};
+
+        AscendingSequence as = new AscendingSequence();
+        int[] actualResult = as.ascendingSequence(start, end, step);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+    
+}
